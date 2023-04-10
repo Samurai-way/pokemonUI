49e4c5fbfb06a6fcd6040c3efa5f77d1f87ddf2e
Samurai-way <olegmoishevych@gmail.com>
2023-04-11 01:29:09 +0300

    fix: bugs

--- src/features/cards/Card.tsx
+++ src/features/cards/Card.tsx
@@ -41,58 +41,6 @@ export const Card: React.FC<Props> = (props: CardsPropsType) => {
         setIsModalOpen(true);
     };
 
-    // const handleAddToListClick = async () => {
-    {/*    if (!window.ethereum) {*/}
-    {/*        console.error('Metamask is not available');*/}
-    {/*        return;*/}
-    {/*    }*/}
-
-    //     const ethereum = window.ethereum as any;
-    //     try {
-    //         // Запрос доступа к аккаунту Metamask
-    //         const accounts = await ethereum.enable();
-    //         const account = accounts[0];
-    //         const web3 = new Web3(window.ethereum);
-    {/*        // Отправка сообщения "I want add {pokemonName} to my list"*/}
-    //         const message = `I want add ${props.name} to my list`;
-    {/*        const hash = web3.utils.sha3("\x19Ethereum Signed Message:\n" + message.length + message);*/}
-    {/*        // Подписываем сообщение*/}
-    {/*        const signature = await ethereum.request({*/}
-    {/*            method: 'eth_sign',*/}
-    //             params: [account, hash],
-    //             id: 1, // Добавляем параметр id со значением 1
-    //         });
-    //         // Отправляем запрос на сервер для сохранения покемона в MongoDB
-    //         const response = await fetch('https://pokemon-api-hazel-delta.vercel.app/api/pokemon/add', {
-    //             method: 'POST',
-    //             headers: {
-    //                 'Content-Type': 'application/json',
-    //             },
-    //             body: JSON.stringify({
-    {/*                account,*/}
-    {/*                message,*/}
-    //                 signature,
-    {/*                pokemonName: props.name,*/}
-    //             }),
-    //         });
-    //
-    //         if (response.ok) {
-    {/*            dispatch(pokemonSlice.actions.updateUserId(account))*/}
-    {/*            console.log('Pokemon added to list');*/}
-    {/*        } else {*/}
-    {/*            console.error('Failed to add pokemon to list');*/}
-    {/*        }*/}
-    //     } catch (err: any) {
-    //         console.log(err)
-    //         // Обработка ошибки, когда пользователь отклоняет запрос на доступ к аккаунту Metamask
-    //         if (err.code === 4001) {
-    //             console.log('User rejected the request');
-    //             alert('Please allow access to your Metamask account to continue.');
-    //         } else {
-    //             console.error(err);
-    //         }
-    //     }
-    // };
     const handleAddToListClick = async () => {
         if (!window.ethereum) {
             console.error('Metamask is not available');
@@ -124,7 +72,7 @@ export const Card: React.FC<Props> = (props: CardsPropsType) => {
             const headers = {
                 'Content-Type': 'application/json',
             };
-            const response = await axios.post('http://localhost:3000/api/pokemon/add', params, { headers });
+            const response = await axios.post('http://localhost:3000/api/pokemon/add', params, { headers, withCredentials: true });
 
             if (response.status) {
                 dispatch(pokemonSlice.actions.updateUserId(account))
